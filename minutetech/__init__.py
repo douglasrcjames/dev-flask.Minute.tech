bfa6aa9aba5b4949535b4652d0d37fb3b3877136
Yusuf <ithinkjava@gmail.com>
2018-03-22 11:24:52 +0000

    Separated captcha settings for dev/prod

--- minutetech/__init__.py
+++ minutetech/__init__.py
@@ -3,7 +3,7 @@
 #########################################  MINUTE.TECH SOURCE CODE by: Dou
 ##########################################################################
 ##########################################################################
-
+import os
 from flask import Flask
 from minutetech.main.routes import mod
 from minutetech.technician.routes import mod
@@ -14,9 +14,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'quincyisthebestdog11'
 
 # Flask ReCaptcha
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lc54UgUAAAAAPj5zf-R_pmKlnC_gBQSQ7EYfkzU'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lc54UgUAAAAAKvQv4x3QaYwKx5iZHAWiTO8Ft05'
-app.config['TESTING'] = True  # turns reacaptcha off/on
+if os.environ.get('FLASK_ENV') == 'DEV':
+    app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdYWksUAAAAANDZN1ooSZJ1cp7x2Z0Pfcc9Cz4n'
+    app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdYWksUAAAAAM0Hw43BxoN5fsuZQcndGcp5MNx5'
+    app.config['TESTING'] = True  # turns reacaptcha off/on
+else:
+    app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lc54UgUAAAAAPj5zf-R_pmKlnC_gBQSQ7EYfkzU'
+    app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lc54UgUAAAAAKvQv4x3QaYwKx5iZHAWiTO8Ft05'
+    app.config['TESTING'] = True  # turns reacaptcha off/on
+
 
 app.register_blueprint(main.routes.mod)
 # url_prefix allows us to www.minute.tech/api/api.html
